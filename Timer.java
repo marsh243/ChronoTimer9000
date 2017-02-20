c7352c06d71c490bc02d3a99c89cf803d1d7e79b
waterworldwasok <ajhonts@uwm.edu>
2017-02-19 23:05:20 -0600

    Timer
    Timer class biotch

---
+++ Timer.java
@@ -0,0 +1,11 @@
+
+public class Timer {
+
+	//create arrays for channels 
+	//calculate race time for a particular racer
+	//Whole race time first race begin last race finish
+	//what event occured on each channel
+	//reset channels button
+	//DNF and cancel options
+	
+}
