05a3c14d9e12012fa48f91e15d1025c378e0a865
marsh243 <marsh243@uwm.edu>
2017-02-28 12:21:36 -0600

    Added Athlete functionality.

--- Athlete.java
+++ Athlete.java
@@ -1,5 +1,20 @@
 
 public class Athlete {
-	//this is where athlete goes
-	//this is a poop
+	private String name, time
+	public Athlete(String name, time){
+		this.name = name;
+		this.time = time;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public String getTime(){
+		return this.time;
+	}
+	
+	public void toString(){
+		System.out.println(name+"\n"+time);
+	}
 }
