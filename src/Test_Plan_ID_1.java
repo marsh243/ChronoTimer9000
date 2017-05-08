33ac26c3a3106ff5968ea6fb6daea0accb39720f
Unknown <nmbrownell1@gmail.com>
2017-05-08 18:17:37 -0500

    Started Test Case adding

---
+++ src/Test_Plan_ID_1.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class Test_Plan_ID_1 {
+
+	@Test
+	public void ID_1_1() {
+		ArrayList<Athlete> a = new ArrayList<Athlete>();
+	}
+	
+	@Test
+	public void ID_1_2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void ID_1_3() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void ID_1_4() {
+		fail("Not yet implemented");
+	}
+
+}
